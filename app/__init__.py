74040b5fce636023368452697effcd023b955120
singularitea <bvscott66@gmail.com>
2019-03-05 19:40:14 +0800

    up to chapter 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLALCHEMY
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLALCHEMY(app)
+migrate = Migrate(app, db)
 
-from app import routes
+
+from app import routes, models
